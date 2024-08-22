9e05e164df8d45c51f78a63c073df56b279f4e8e
Mayank grover <maygro1609@gmail.com>
2024-08-22 23:59:24 +0530

    Multiply feature added

--- demo.java
+++ demo.java
@@ -1,8 +1,12 @@
 public class demo{
+    public static int multiply(int a,int b){
+        return a*b;
+    }
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
         int c = a + b;
         System.out.println("The result of the addition is :" + c);
+        System.out.println(multiply(1,2));
     }
 }
